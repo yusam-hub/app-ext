16d6b878ecd46b098b6d089a47efcd021ea81caf
yusam <yusam@yandex.ru>
2023-03-05 15:23:44 +0400

    #changed

--- src/SymfonyExt/ControllerKernel.php
+++ src/SymfonyExt/ControllerKernel.php
@@ -15,8 +15,15 @@ use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Router;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Routing\Loader\PhpFileLoader;
-class ControllerKernel
+use YusamHub\AppExt\Interfaces\GetSetConsoleInterface;
+use YusamHub\AppExt\Interfaces\GetSetLoggerInterface;
+use YusamHub\AppExt\Traits\GetSetConsoleTrait;
+use YusamHub\AppExt\Traits\GetSetLoggerTrait;
+
+class ControllerKernel implements GetSetLoggerInterface, GetSetConsoleInterface
 {
+    use GetSetLoggerTrait;
+    use GetSetConsoleTrait;
     protected string $routeDir;
     protected Request $request;
     protected string $phpFile;
@@ -40,89 +47,54 @@ class ControllerKernel
         $this->runInReactHttp = $runInReactHttp;
         $this->requestContext = new RequestContext();
         $this->requestContext->fromRequest($request);
-        $this->router = new Router(
-            new PhpFileLoader(
-                new FileLocator($this->routeDir)
-            ),
-            $this->phpFile,
-            [
-                // 'cache_dir' => app()->getRootDir() . '/storage/app/caches/routes'
-            ],
-            $this->requestContext
-        );
-    }
-
-    /**
-     * @return Router
-     */
-    public function getRouter(): Router
-    {
-        return $this->router;
-    }
-
-    /**
-     * @return string
-     */
-    public function getRouteDir(): string
-    {
-        return $this->routeDir;
-    }
-
-    /**
-     * @return Request
-     */
-    public function getRequest(): Request
-    {
-        return $this->request;
-    }
-
-    /**
-     * @return RequestContext
-     */
-    public function getRequestContext(): RequestContext
-    {
-        return $this->requestContext;
-    }
-
-    /**
-     * @return array
-     */
-    public function getRouteMatch(): array
-    {
-        return $this->router->match($this->requestContext->getPathInfo());
     }
 
     /**
      * @return \React\Http\Message\Response|Response
-     * @throws \Exception
+     * @throws \Exception|\Throwable
      */
     public function fetchResponse()
     {
-        $dispatcher = new EventDispatcher();
-
-        $dispatcher->addSubscriber(
-            new RouterListener(
-                new UrlMatcher(
-                    $this->router->getRouteCollection(),
-                    new RequestContext()
-                ),
-                new RequestStack()
-            )
-        );
+        $this->debug("REQUEST: " . $this->request->getMethod() . ' ' . $this->request->getRequestUri(), [
+            'query' => $this->request->getQueryString(),
+            'params' => $this->request->attributes->all(),
+        ]);
 
         try {
+            $this->router = new Router(
+                new PhpFileLoader(
+                    new FileLocator($this->routeDir)
+                ),
+                $this->phpFile,
+                [
+                    // 'cache_dir' => app()->getRootDir() . '/storage/app/caches/routes'
+                ],
+                $this->requestContext
+            );
+
+            $dispatcher = new EventDispatcher();
+
+            $dispatcher->addSubscriber(
+                new RouterListener(
+                    new UrlMatcher(
+                        $this->router->getRouteCollection(),
+                        new RequestContext()
+                    ),
+                    new RequestStack()
+                )
+            );
+
             $this->httpKernel = new HttpKernel($dispatcher, new ControllerResolver(), new RequestStack(), new ArgumentResolver());
 
             $response = $this->httpKernel->handle($this->request);
 
-        } catch (\Throwable $e) {
-            if ($e instanceof NotFoundHttpException) {
-                $response = new Response('Not Found', 404);
-                //$this->httpServer->debug(sprintf("RESPONSE (%d): %s", $responseStatusCode, $responseStatusMessage));
-            } else {
-                throw $e;
-            }
+        } catch (NotFoundHttpException $e) {
+
+            $response = new Response('Not Found', 404);//todo: call controller for NotFound
+
         }
+
+        $this->debug(sprintf("RESPONSE (%d): %s", $response->getStatusCode(), $response->getContent()));
 
         if ($this->runInReactHttp) {
             return new \React\Http\Message\Response(
