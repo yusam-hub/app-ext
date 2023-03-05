59ccf8d2318ae304bca6a0f209f4bd9ea2f39a6c
yusam <yusam@yandex.ru>
2023-03-05 14:48:34 +0400

    #changed

--- src/Interfaces/GetSetLoggerInterface.php
+++ src/Interfaces/GetSetLoggerInterface.php
@@ -8,4 +8,6 @@ interface GetSetLoggerInterface extends LoggerInterface
 {
     public function getLogger(): ?LoggerInterface;
     public function setLogger(?LoggerInterface $logger): void;
+    public function getConsoleOutputEnabled(): bool;
+    public function setConsoleOutputEnabled(bool $value): void;
 }
--- src/Traits/GetSetLoggerTrait.php
+++ src/Traits/GetSetLoggerTrait.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Output\Output;
 trait GetSetLoggerTrait
 {
     private ?LoggerInterface $logger = null;
+    private bool $consoleOutputEnabled = false;
 
     public function getLogger(): ?LoggerInterface
     {
@@ -20,9 +21,19 @@ trait GetSetLoggerTrait
         $this->logger = $logger;
     }
 
-    private function doConsoleOutput($level, $message, array $context = array(), bool $consoleOutputEnabled = false): void
+    public function getConsoleOutputEnabled(): bool
     {
-        if ($consoleOutputEnabled && isset($this->consoleOutput) &&  $this->consoleOutput instanceof Output) {
+        return $this->consoleOutputEnabled;
+    }
+
+    public function setConsoleOutputEnabled(bool $value): void
+    {
+        $this->consoleOutputEnabled = $value;
+    }
+
+    private function doConsoleOutput($level, $message, array $context = array()): void
+    {
+        if ($this->consoleOutputEnabled && isset($this->consoleOutput) &&  $this->consoleOutput instanceof Output) {
             $this->consoleOutput->writeln(
                 sprintf(
                     "[%s][%s] %s%s",
@@ -35,57 +46,57 @@ trait GetSetLoggerTrait
         }
     }
 
-    public function emergency($message, array $context = array(), bool $consoleOutputEnabled = false)
+    public function emergency($message, array $context = array())
     {
         $this->logger->emergency($message, $context);
-        $this->doConsoleOutput(LogLevel::EMERGENCY, $message, $context, $consoleOutputEnabled);
+        $this->doConsoleOutput(LogLevel::EMERGENCY, $message, $context);
     }
 
-    public function alert($message, array $context = array(), bool $consoleOutputEnabled = false)
+    public function alert($message, array $context = array())
     {
         $this->logger->alert($message, $context);
-        $this->doConsoleOutput(LogLevel::ALERT, $message, $context, $consoleOutputEnabled);
+        $this->doConsoleOutput(LogLevel::ALERT, $message, $context);
     }
 
-    public function critical($message, array $context = array(), bool $consoleOutputEnabled = false)
+    public function critical($message, array $context = array())
     {
         $this->logger->critical($message, $context);
-        $this->doConsoleOutput(LogLevel::CRITICAL, $message, $context, $consoleOutputEnabled);
+        $this->doConsoleOutput(LogLevel::CRITICAL, $message, $context);
     }
 
-    public function error($message, array $context = array(), bool $consoleOutputEnabled = false)
+    public function error($message, array $context = array())
     {
         $this->logger->error($message, $context);
-        $this->doConsoleOutput(LogLevel::ERROR, $message, $context, $consoleOutputEnabled);
+        $this->doConsoleOutput(LogLevel::ERROR, $message, $context);
     }
 
-    public function warning($message, array $context = array(), bool $consoleOutputEnabled = false)
+    public function warning($message, array $context = array())
     {
         $this->logger->warning($message, $context);
-        $this->doConsoleOutput(LogLevel::WARNING, $message, $context, $consoleOutputEnabled);
+        $this->doConsoleOutput(LogLevel::WARNING, $message, $context);
     }
 
-    public function notice($message, array $context = array(), bool $consoleOutputEnabled = false)
+    public function notice($message, array $context = array())
     {
         $this->logger->notice($message, $context);
-        $this->doConsoleOutput(LogLevel::NOTICE, $message, $context, $consoleOutputEnabled);
+        $this->doConsoleOutput(LogLevel::NOTICE, $message, $context);
     }
 
-    public function info($message, array $context = array(), bool $consoleOutputEnabled = false)
+    public function info($message, array $context = array())
     {
         $this->logger->info($message, $context);
-        $this->doConsoleOutput(LogLevel::INFO, $message, $context, $consoleOutputEnabled);
+        $this->doConsoleOutput(LogLevel::INFO, $message, $context);
     }
 
-    public function debug($message, array $context = array(), bool $consoleOutputEnabled = false)
+    public function debug($message, array $context = array())
     {
         $this->logger->debug($message, $context);
-        $this->doConsoleOutput(LogLevel::DEBUG, $message, $context, $consoleOutputEnabled);
+        $this->doConsoleOutput(LogLevel::DEBUG, $message, $context);
     }
 
-    public function log($level, $message, array $context = array(), bool $consoleOutputEnabled = false)
+    public function log($level, $message, array $context = array())
     {
         $this->logger->log($level, $message, $context);
-        $this->doConsoleOutput($level, $message, $context, $consoleOutputEnabled);
+        $this->doConsoleOutput($level, $message, $context);
     }
 }
