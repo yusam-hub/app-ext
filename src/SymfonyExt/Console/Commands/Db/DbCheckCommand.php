b40c4a2bd81e01578ead4d240bbc73fd46f7d33f
yusam <yusam@yandex.ru>
2023-03-08 07:59:28 +0400

    #changed

---
+++ src/SymfonyExt/Console/Commands/Db/DbCheckCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace YusamHub\AppExt\SymfonyExt\Console\Commands\Db;
+
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use YusamHub\AppExt\SymfonyExt\Console\Commands\BaseConsoleCommand;
+class DbCheckCommand extends BaseConsoleCommand
+{
+    protected function configure(): void
+    {
+        $this
+            ->setName('db:check')
+            ->setDescription('db:check:description')
+            ->setHelp('db:check:help')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln("");
+
+        $phpDt = date("Y-m-d H:i:s");
+        $output->writeln($this->tagGreen(sprintf('PHP: %s',$phpDt)));
+
+        $out = [];
+
+        foreach(db()->getConnectionNames() as $connectionName)
+        {
+            try {
+
+                $row = db()->{$connectionName}->fetchOne('SELECT NOW() as dt, DATABASE() as dbName');
+
+                $out[] = [
+                    $connectionName, $row['dbName'], ($row['dt'] != $phpDt) ? $this->tagYellow($row['dt']) : $row['dt'], ($row['dt'] != $phpDt) ? "Invalid date time between mysql & php" : $this->tagGreen('SUCCESS')
+                ];
+
+            } catch (\Throwable $e) {
+
+                $out[] = [
+                    $connectionName, '-', '-', $this->tagRed('ERROR: ' . $e->getMessage())
+                ];
+
+            }
+        }
+
+        $table = new Table($output);
+        $table
+            ->setHeaders(['Connection', 'Db Name','DateTime','Info'])
+            ->setRows($out)
+        ;
+        $table->render();
+
+        return self::SUCCESS;
+    }
+}
---
+++ src/SymfonyExt/Console/Commands/Db/DbMigrateCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace YusamHub\AppExt\SymfonyExt\Console\Commands\Db;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use YusamHub\AppExt\SymfonyExt\Console\Commands\BaseConsoleCommand;
+use YusamHub\DbExt\PdoExtMigrations;
+class DbMigrateCommand extends BaseConsoleCommand
+{
+    protected function configure(): void
+    {
+        $this
+            ->setName('db:migrate')
+            ->setDescription('db:migrate:description')
+            ->setHelp('db:migrate:help')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        foreach(db()->getConnectionNames() as $connectionName)
+        {
+            $migrations = new PdoExtMigrations(db()->{$connectionName}, app()->getDatabaseDir('/migrations/' . $connectionName), app()->getStorageDir('/app/migrations/migrations_' . $connectionName . '.txt'));
+            $migrations->migrate();
+        }
+
+        return self::SUCCESS;
+    }
+}
