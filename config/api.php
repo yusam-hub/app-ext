d62b140c3d34f035e5cc9f204bed171662de8db6
yusam <yusam@yandex.ru>
2023-03-27 11:34:50 +0400

    changed

--- config/api.php
+++ config/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use YusamHub\DbExt\Interfaces\PdoExtModelInterface;
+
 return [
     'publicSwaggerUiDir' => app_ext()->getPublicDir('/swagger-ui'),
     'publicSwaggerUiUri' => '/swagger-ui',
@@ -17,27 +19,6 @@ return [
         \Symfony\Component\HttpFoundation\Request $request
     )
     {
-        if ($request->getRequestUri() === '/') {
-            return null;
-        }
-
-        $debugTokens = (array) app_ext_config('api.debugTokens');
-
-        if (empty($debugTokens)) {
-            return null;
-        }
-
-        $tokenValue = (string) $request->headers->get(app_ext_config('api.tokenKeyName'));
-
-        if (in_array($tokenValue, array_keys($debugTokens))) {
-            return intval($debugTokens[$tokenValue]);
-        }
-
-        $apiUserModel = \YusamHub\AppExt\Db\Model\ApiUserModel\ApiUserModel::findModelByAttributes($httpController->getPdoExtKernel(), ['apiToken' => $tokenValue]);
-        if (!is_null($apiUserModel)) {
-            return $apiUserModel;
-        }
-
         throw new \YusamHub\AppExt\Exceptions\HttpUnauthorizedAppExtRuntimeException([
             'detail' => 'Invalid token value'
         ]);
@@ -46,31 +27,12 @@ return [
         \YusamHub\AppExt\Traits\Interfaces\GetSetHttpControllerInterface $httpController,
         \Symfony\Component\HttpFoundation\Request $request,
         int $apiAuthorizedId,
-        ?\YusamHub\AppExt\Db\Model\ApiUserModel\ApiUserModel $apiUserModel
+        $model
     )
     {
-        $signValue = (string) $request->headers->get(app_ext_config('api.signKeyName'));
-
-        if (is_null($apiUserModel)) {
-
-            $debugSigns = (array) app_ext_config('api.debugSigns');
-
-            if (isset($debugSigns[$apiAuthorizedId]) && $signValue !== $debugSigns[$apiAuthorizedId])
-            {
-                throw new \YusamHub\AppExt\Exceptions\HttpUnauthorizedAppExtRuntimeException([
-                    'detail' => 'Invalid debug sign value',
-                ]);
-            }
-
-            return;
-        }
-
-        if ($signValue !== $apiUserModel->apiSign)
-        {
-            throw new \YusamHub\AppExt\Exceptions\HttpUnauthorizedAppExtRuntimeException([
-                'detail' => 'Invalid sign value',
-            ]);
-        }
+        throw new \YusamHub\AppExt\Exceptions\HttpUnauthorizedAppExtRuntimeException([
+            'detail' => 'Invalid sign value',
+        ]);
     },
     //'infoTitle' => 'Api %s Server',
     //'infoVersion' => '1.0.0',
