d59cb72220d250338ab17edc5af320f206327370
yusam <yusam@yandex.ru>
2023-08-04 00:44:23 +0300

    #changed

--- src/Redis/RedisCacheUseFresh.php
+++ src/Redis/RedisCacheUseFresh.php
@@ -7,6 +7,26 @@ use YusamHub\RedisExt\RedisExt;
 
 class RedisCacheUseFresh
 {
+    const CACHE_TTL_5_SECONDS = 5;
+    const CACHE_TTL_10_SECONDS = 10;
+    const CACHE_TTL_15_SECONDS = 15;
+    const CACHE_TTL_20_SECONDS = 20;
+    const CACHE_TTL_MINUTE = 60;
+    const CACHE_TTL_2_MINUTES = self::CACHE_TTL_MINUTE * 2;
+    const CACHE_TTL_3_MINUTES = self::CACHE_TTL_MINUTE * 3;
+    const CACHE_TTL_5_MINUTES = self::CACHE_TTL_MINUTE * 5;
+    const CACHE_TTL_10_MINUTES = self::CACHE_TTL_MINUTE * 10;
+    const CACHE_TTL_15_MINUTES = self::CACHE_TTL_MINUTE * 15;
+    const CACHE_TTL_HOUR = self::CACHE_TTL_MINUTE * 60;
+    const CACHE_TTL_3_HOURS = self::CACHE_TTL_HOUR * 3;
+    const CACHE_TTL_6_HOURS = self::CACHE_TTL_HOUR * 6;
+    const CACHE_TTL_DAY = self::CACHE_TTL_HOUR * 24;
+    const CACHE_TTL_7_DAY = self::CACHE_TTL_DAY * 7;
+    const CACHE_TTL_MONTH = self::CACHE_TTL_DAY * 30;
+    const CACHE_TTL_3_MONTH = self::CACHE_TTL_MONTH * 3;
+    const CACHE_TTL_6_MONTH = self::CACHE_TTL_MONTH * 6;
+    const CACHE_TTL_YEAR = self::CACHE_TTL_DAY * 365;
+
     public static function rememberExt(
         RedisExt $redisExt,
         LoggerInterface $logger,
